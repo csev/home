39a8b6407ade206462ab9b23344d5dbb0585a852
Charles Severance <csev@umich.edu>
2026-02-25 20:48:32 -0500

    Updates

--- index.php
+++ index.php
@@ -3,122 +3,185 @@
 <head>
   <title>Dr. Charles R. Severance Home Page</title>
   <meta name="verify-v1" content="WQuA2ZPREiCyTlgNh/fv0jvzKJxrpzlagjiPaakSNH0=" />
-<style type="text/css">
-body { background: black; font-family: Arial,Helvetica,Verdana,Sans-Serif; color: white;}
-body table { font-size: 11pt; }
-a:link, a:visited, a:active { color: gray; text-decoration: none; font-weight: bold}
-a:hover { color: orange; }
-a:focus-visible { color: orange; outline: 2px solid orange; outline-offset: 2px; }
-.skip-link { position: absolute; top: -40px; left: 0; background: orange; color: black; padding: 8px; z-index: 100; }
-.skip-link:focus { top: 0; }
-.visually-hidden { position: absolute; width: 1px; height: 1px; padding: 0; margin: -1px; overflow: hidden; clip: rect(0,0,0,0); white-space: nowrap; border: 0; }
-strong, h2 { color: orange; font-size: inherit; font-weight: bold; margin: 0; }
-h2 + p { margin-top: 0; }
-em {color: yellow; font-style: normal;}
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="google-translate-customization" content="502d2c1a267d1206-8efe060c714e194c-g94a06c6c571083ae-11">
+  <link rel="alternate" type="application/rss+xml" title="RSS" href="https://www.dr-chuck.com/csev-blog/index.rdf" />
+  <link rel="alternate" type="application/atom+xml" title="Atom" href="https://www.dr-chuck.com/csev-blog/atom.xml" />
+  <style>
+    * { box-sizing: border-box; }
+    body {
+      background: black;
+      font-family: Arial, Helvetica, Verdana, sans-serif;
+      color: white;
+      font-size: 11pt;
+      margin: 0;
+      padding: 0;
+    }
+    a:link, a:visited, a:active {
+      color: gray;
+      text-decoration: none;
+      font-weight: bold;
+    }
+    a:hover { color: orange; }
+    a:focus-visible {
+      color: orange;
+      outline: 2px solid orange;
+      outline-offset: 2px;
+    }
+    .skip-link {
+      position: absolute;
+      top: -40px;
+      left: 0;
+      background: orange;
+      color: black !important;
+      padding: 8px;
+      z-index: 100;
+    }
+    .skip-link:link, .skip-link:visited, .skip-link:active,
+    .skip-link:hover, .skip-link:focus, .skip-link:focus-visible {
+      color: black !important;
+    }
+    .skip-link:focus { top: 0; }
+    .visually-hidden {
+      position: absolute;
+      width: 1px;
+      height: 1px;
+      padding: 0;
+      margin: -1px;
+      overflow: hidden;
+      clip: rect(0,0,0,0);
+      white-space: nowrap;
+      border: 0;
+    }
+    strong, h2 {
+      color: orange;
+      font-size: inherit;
+      font-weight: bold;
+      margin: 0 0 0.25em 0;
+    }
+    h2 + p, h2 + ul { margin-top: 0; }
+    em { color: yellow; font-style: normal; }
 
-</style>
-<link rel="alternate" type="application/rss+xml" title="RSS" href="https://www.dr-chuck.com/csev-blog/index.rdf" />
-<link rel="alternate" type="application/atom+xml" title="Atom" href="https://www.dr-chuck.com/csev-blog/atom.xml" />
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="google-translate-customization" content="502d2c1a267d1206-8efe060c714e194c-g94a06c6c571083ae-11"></meta>
+    .layout {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 2rem;
+      max-width: 900px;
+      margin: 0 auto;
+      padding: 1rem;
+    }
+    .sidebar {
+      flex: 0 0 180px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      text-align: center;
+    }
+    .sidebar nav ul {
+      list-style: none;
+      padding: 0;
+      margin: 1rem 0 0 0;
+    }
+    .sidebar nav li {
+      margin: 0.5rem 0;
+    }
+    .sidebar img {
+      border: 2px solid white;
+      width: 160px;
+      height: auto;
+      display: block;
+    }
+    .main-content {
+      flex: 1;
+      min-width: 280px;
+    }
+    .main-content ul {
+      list-style: none;
+      padding: 0;
+      margin: 0 0 1rem 0;
+    }
+    .main-content li {
+      margin: 0.25em 0;
+    }
+    .main-content li::before {
+      content: "\00a0\00a0";
+    }
+  </style>
 </head>
 <body>
-<a href="#main-content" class="skip-link">Skip to main content</a>
-<table border="0" role="presentation">
-<tr>
-<td align=center valign=top width=180>
-<a href="https://www.learnerprivacy.org/" aria-label="Photo of Dr. Charles Severance - LearnerPrivacy.org">
-<img align="center" src="Chuck_Square_B&amp;W.jpg" width="160" style="border: white 2px solid;" alt="Photo of Dr. Charles R. Severance">
-</a>
-<br>
-<br> <a href="https://www.si.umich.edu/" target="_blank" rel="noopener noreferrer">School of Information</a>
-<br> <a href="https://online.dr-chuck.com/">Free Courses</a>
-<br> <a href="https://podcast.dr-chuck.com/" target="_blank" rel="noopener noreferrer">Podcast</a>
-<br> <a href="https://youtube.dr-chuck.com/" target="_blank" rel="noopener noreferrer">YouTube</a>
-<br/> <a href="https://www.linkedin.com/in/charlesseverance/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-<br/> <a target="_blank" rel="noopener noreferrer" href="https://www.coursera.org/instructor/drchuck">My Coursera Page</a>
-<br> <a target="_blank" rel="noopener noreferrer" href="https://amzn.to/1K5Q81K">Amazon Author Page</a>
-<br> <a href="https://www.dr-chuck.com/dr-chuck/resume/speaking.htm" target="_blank" rel="noopener noreferrer">Keynote Speaker</a>
-<br/> <a href="https://www.slideshare.net/csev" target="_blank" rel="noopener noreferrer">Slideshare</a>
-<br> <a href="/dr-chuck/resume/index.htm" target="_blank" rel="noopener noreferrer">Resume and Bio</a>
-<br> <a href="https://www.youtube.com/playlist?list=PLHJB2bhmgB7dFuY7HmrXLj5BmHGKTD-3R" target="_blank" rel="noopener noreferrer">IEEE Video Interviews</a>
-<br> <a href="https://www.dr-chuck.com/csev-blog/">Blog</a>
-<br> <a href="http://afs.dr-chuck.com/papers/" target="_blank" rel="noopener noreferrer">Chuck's Papers</a>
-<br> <a href="https://vimeo.com/drchuck/videos" target="_blank" rel="noopener noreferrer">Video on Vimeo</a>
-</td>
-<td valign="top" id="main-content" tabindex="-1">
-<h1 class="visually-hidden">Dr. Charles R. Severance Home Page</h1>
+  <header role="banner">
+    <a href="#main-content" class="skip-link">Skip to main content</a>
+  </header>
 
-<h2>Free Courses / Educational Material</h2>
-<p>
-<br> &nbsp;
-<a href="https://ihts.pr4e.com/" target="_blank" rel="noopener noreferrer">Internet History, Technnology and Security</a>
-<br> &nbsp;
-<a href="https://www.py4e.com/" target="_blank" rel="noopener noreferrer">Python for Everybody</a>
-<br> &nbsp;
-<a href="https://www.dj4e.com/" target="_blank" rel="noopener noreferrer">Django for Everybody</a>
-<br> &nbsp;
-<a href="https://www.wa4e.com/" target="_blank" rel="noopener noreferrer">Web Applications for Everybody</a> (PHP/SQL)
-<br> &nbsp;
-<a href="https://www.pg4e.com/" target="_blank" rel="noopener noreferrer">PostgreSQL for Everybody</a>
-<br> &nbsp;
-<a href="https://www.cc4e.com/" target="_blank" rel="noopener noreferrer">C Programming for Everybody</a>
-<br> &nbsp;
-<a href="https://www.ca4e.com/" target="_blank" rel="noopener noreferrer">Computer Architecture for Everybody</a>
-<br> &nbsp;
-<a href="office/index.htm" target="_blank" rel="noopener noreferrer">Dr. Chuck Office Hours Around the World</a> <br/>
-</p>
-<h2>Software</h2>
-<p>
-<br> &nbsp;
-<a href="https://www.sakaiproject.org/" target="_blank" rel="noopener noreferrer">The Sakai Collaboration
-and Learning Environment</a>
-<br> &nbsp;
-<a href="https://www.tsugi.org/" target="_blank" rel="noopener noreferrer">Tsugi: A framework for learning tools</a>
-<br> &nbsp;
-<a href="https://www.imsglobal.org/activity/learning-tools-interoperability-lti%C2%AE" target="_blank" rel="noopener noreferrer">Learning Tools Interoperability (LTI)</a>
-</p>
-<h2>Books</h2>
-<p>
-<br> &nbsp; <a href="http://www.py4e.com/book" target="_blank" rel="noopener noreferrer">Python For Everybody: Exploring Data in Python 3</a>  (2016)
-<br> &nbsp; <a href="/sakai-book">Sakai: Building an Open Source Community</a> (2011, 2014)
-<br> &nbsp; <a href="http://www.amazon.com/gp/product/1624311393/ref=as_li_ss_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=1624311393&linkCode=as2&tag=drchu02-20" target="_blank" rel="noopener noreferrer">Raspberry Pi (21st Century Skills Innovation Library)</a> (2013)
-<br> &nbsp; <a href="http://www.amazon.com/gp/product/059680069X/ref=as_li_ss_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=059680069X&linkCode=as2&tag=drchu02-20" target="_blank" rel="noopener noreferrer">Using Google App Engine</a> (O'Reilly 2009)
-<br> &nbsp; <a href="http://www.amazon.com/Performance-Computing-Architectures-Optimization-Benchmarks/dp/156592312X/" target="_blank" rel="noopener noreferrer">High Performance Computing</a> (<a href="http://oreilly.com/catalog/9781565923126/" target="_blank" rel="noopener noreferrer">O'Reilly 1998</a>,  <a href="http://cnx.org/content/col11136/latest/" target="_blank" rel="noopener noreferrer">Connexions 2010</a>)
-</p>
-<h2>Web/Multimedia sites</h2>
-<p>
-<br> &nbsp;
-<a href="https://www.learnerprivacy.org/" target="_blank" rel="noopener noreferrer">My LearnerPrivacy.org podcast</a>
-<br> &nbsp;
-<a href="http://www.youtube.com/playlist?list=PLHJB2bhmgB7dFuY7HmrXLj5BmHGKTD-3R" target="_blank" rel="noopener noreferrer">
-IEEE Computer - Computing Conversations Interviews</a>
-(2011-2016)
-<br> &nbsp;
-<a href="https://www.vimeo.com/17207620" target="_blank" rel="noopener noreferrer">
-Dr. Chuck sings the blues </a> (2008)
-<br> &nbsp;
-<a href="https://www.youtube.com/watch?v=BVKpW02hsrU" target="_blank" rel="noopener noreferrer">
-Dr. Chuck goes motocross racing</a> (2007)
-<br> &nbsp;
-<a href="https://www.youtube.com/watch?v=sa2WsgCvn7c" target="_blank" rel="noopener noreferrer">
-A Film About Brent and His ATV
-</a> (2005)
-<br> &nbsp;
-<a href="https://www.vimeo.com/17213019" target="_blank" rel="noopener noreferrer">
-Audition Tape</a> (2003) for TechTV which was
-rejected :(.
-<br> &nbsp;
-<a href="https://www.youtube.com/watch?v=FJ078sO35M0" target="_blank" rel="noopener noreferrer">
-Dr. Chuck goes stock car racing</a> (2002)
-<br> &nbsp;
-<a href="http://afs.dr-chuck.com/citoolkit" target="_blank" rel="noopener noreferrer">
-The Community Information Toolkit</a> - A project to provide public libraries and 
-other organizations a start on using Internet in Commmunity Networking. (1999)
-</td>
-</tr>
-</table>
-<br> &nbsp;
+  <div class="layout" role="presentation">
+    <aside class="sidebar" role="complementary" aria-label="Profile and links">
+      <a href="https://www.learnerprivacy.org/" aria-label="Photo of Dr. Charles Severance - LearnerPrivacy.org">
+        <img src="Chuck_Square_B&amp;W.jpg" alt="Photo of Dr. Charles R. Severance">
+      </a>
+      <nav aria-label="Quick links">
+        <ul>
+          <li><a href="https://www.si.umich.edu/" target="_blank" rel="noopener noreferrer">School of Information</a></li>
+          <li><a href="https://online.dr-chuck.com/">Free Courses</a></li>
+          <li><a href="https://podcast.dr-chuck.com/" target="_blank" rel="noopener noreferrer">Podcast</a></li>
+          <li><a href="https://youtube.dr-chuck.com/" target="_blank" rel="noopener noreferrer">YouTube</a></li>
+          <li><a href="https://www.linkedin.com/in/charlesseverance/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+          <li><a href="https://www.coursera.org/instructor/drchuck" target="_blank" rel="noopener noreferrer">My Coursera Page</a></li>
+          <li><a href="https://amzn.to/1K5Q81K" target="_blank" rel="noopener noreferrer">Amazon Author Page</a></li>
+          <li><a href="https://www.dr-chuck.com/dr-chuck/resume/speaking.htm" target="_blank" rel="noopener noreferrer">Keynote Speaker</a></li>
+          <li><a href="https://www.slideshare.net/csev" target="_blank" rel="noopener noreferrer">Slideshare</a></li>
+          <li><a href="/dr-chuck/resume/index.htm" target="_blank" rel="noopener noreferrer">Resume and Bio</a></li>
+          <li><a href="https://www.youtube.com/playlist?list=PLHJB2bhmgB7dFuY7HmrXLj5BmHGKTD-3R" target="_blank" rel="noopener noreferrer">IEEE Video Interviews</a></li>
+          <li><a href="https://www.dr-chuck.com/csev-blog/">Blog</a></li>
+          <li><a href="http://afs.dr-chuck.com/papers/" target="_blank" rel="noopener noreferrer">Chuck's Papers</a></li>
+          <li><a href="https://vimeo.com/drchuck/videos" target="_blank" rel="noopener noreferrer">Video on Vimeo</a></li>
+        </ul>
+      </nav>
+    </aside>
+
+    <main class="main-content" id="main-content" tabindex="-1" role="main">
+      <h1 class="visually-hidden">Dr. Charles R. Severance Home Page</h1>
+
+      <h2>Free Courses / Educational Material</h2>
+      <ul>
+        <li><a href="https://ihts.pr4e.com/" target="_blank" rel="noopener noreferrer">Internet History, Technnology and Security</a></li>
+        <li><a href="https://www.py4e.com/" target="_blank" rel="noopener noreferrer">Python for Everybody</a></li>
+        <li><a href="https://www.dj4e.com/" target="_blank" rel="noopener noreferrer">Django for Everybody</a></li>
+        <li><a href="https://www.wa4e.com/" target="_blank" rel="noopener noreferrer">Web Applications for Everybody</a> (PHP/SQL)</li>
+        <li><a href="https://www.pg4e.com/" target="_blank" rel="noopener noreferrer">PostgreSQL for Everybody</a></li>
+        <li><a href="https://www.cc4e.com/" target="_blank" rel="noopener noreferrer">C Programming for Everybody</a></li>
+        <li><a href="https://www.ca4e.com/" target="_blank" rel="noopener noreferrer">Computer Architecture for Everybody</a></li>
+        <li><a href="office/index.htm" target="_blank" rel="noopener noreferrer">Dr. Chuck Office Hours Around the World</a></li>
+      </ul>
+
+      <h2>Software</h2>
+      <ul>
+        <li><a href="https://www.sakaiproject.org/" target="_blank" rel="noopener noreferrer">The Sakai Collaboration and Learning Environment</a></li>
+        <li><a href="https://www.tsugi.org/" target="_blank" rel="noopener noreferrer">Tsugi: A framework for learning tools</a></li>
+        <li><a href="https://www.imsglobal.org/activity/learning-tools-interoperability-lti%C2%AE" target="_blank" rel="noopener noreferrer">Learning Tools Interoperability (LTI)</a></li>
+      </ul>
+
+      <h2>Books</h2>
+      <ul>
+        <li><a href="http://www.py4e.com/book" target="_blank" rel="noopener noreferrer">Python For Everybody: Exploring Data in Python 3</a> (2016)</li>
+        <li><a href="/sakai-book">Sakai: Building an Open Source Community</a> (2011, 2014)</li>
+        <li><a href="http://www.amazon.com/gp/product/1624311393/ref=as_li_ss_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=1624311393&linkCode=as2&tag=drchu02-20" target="_blank" rel="noopener noreferrer">Raspberry Pi (21st Century Skills Innovation Library)</a> (2013)</li>
+        <li><a href="http://www.amazon.com/gp/product/059680069X/ref=as_li_ss_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=059680069X&linkCode=as2&tag=drchu02-20" target="_blank" rel="noopener noreferrer">Using Google App Engine</a> (O'Reilly 2009)</li>
+        <li><a href="http://www.amazon.com/Performance-Computing-Architectures-Optimization-Benchmarks/dp/156592312X/" target="_blank" rel="noopener noreferrer">High Performance Computing</a> (<a href="http://oreilly.com/catalog/9781565923126/" target="_blank" rel="noopener noreferrer">O'Reilly 1998</a>, <a href="http://cnx.org/content/col11136/latest/" target="_blank" rel="noopener noreferrer">Connexions 2010</a>)</li>
+      </ul>
+
+      <h2>Web/Multimedia sites</h2>
+      <ul>
+        <li><a href="https://www.learnerprivacy.org/" target="_blank" rel="noopener noreferrer">My LearnerPrivacy.org podcast</a></li>
+        <li><a href="http://www.youtube.com/playlist?list=PLHJB2bhmgB7dFuY7HmrXLj5BmHGKTD-3R" target="_blank" rel="noopener noreferrer">IEEE Computer - Computing Conversations Interviews</a> (2011-2016)</li>
+        <li><a href="https://www.vimeo.com/17207620" target="_blank" rel="noopener noreferrer">Dr. Chuck sings the blues</a> (2008)</li>
+        <li><a href="https://www.youtube.com/watch?v=BVKpW02hsrU" target="_blank" rel="noopener noreferrer">Dr. Chuck goes motocross racing</a> (2007)</li>
+        <li><a href="https://www.youtube.com/watch?v=sa2WsgCvn7c" target="_blank" rel="noopener noreferrer">A Film About Brent and His ATV</a> (2005)</li>
+        <li><a href="https://www.vimeo.com/17213019" target="_blank" rel="noopener noreferrer">Audition Tape</a> (2003) for TechTV which was rejected :(.</li>
+        <li><a href="https://www.youtube.com/watch?v=FJ078sO35M0" target="_blank" rel="noopener noreferrer">Dr. Chuck goes stock car racing</a> (2002)</li>
+        <li><a href="http://afs.dr-chuck.com/citoolkit" target="_blank" rel="noopener noreferrer">The Community Information Toolkit</a> - A project to provide public libraries and other organizations a start on using Internet in Community Networking. (1999)</li>
+      </ul>
+    </main>
+  </div>
 </body>
 </html>
