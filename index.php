56c9ee932a11dd82c54a435f1f5c209ca0f94520
Charles Severance <csev@umich.edu>
2026-02-25 20:37:42 -0500

    Updates

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
   <title>Dr. Charles R. Severance Home Page</title>
   <meta name="verify-v1" content="WQuA2ZPREiCyTlgNh/fv0jvzKJxrpzlagjiPaakSNH0=" />
@@ -6,128 +7,122 @@
 body { background: black; font-family: Arial,Helvetica,Verdana,Sans-Serif; color: white;}
 body table { font-size: 11pt; }
 a:link, a:visited, a:active { color: gray; text-decoration: none; font-weight: bold}
-a:hover { color:orange }
-strong {color: orange; }
+a:hover { color: orange; }
+a:focus { color: orange; outline: 2px solid orange; outline-offset: 2px; }
+.skip-link { position: absolute; top: -40px; left: 0; background: orange; color: black; padding: 8px; z-index: 100; }
+.skip-link:focus { top: 0; }
+.visually-hidden { position: absolute; width: 1px; height: 1px; padding: 0; margin: -1px; overflow: hidden; clip: rect(0,0,0,0); white-space: nowrap; border: 0; }
+strong, h2 { color: orange; font-size: inherit; font-weight: bold; margin: 0; }
+h2 + p { margin-top: 0; }
 em {color: yellow; font-style: normal;}
 
 </style>
 <link rel="alternate" type="application/rss+xml" title="RSS" href="https://www.dr-chuck.com/csev-blog/index.rdf" />
 <link rel="alternate" type="application/atom+xml" title="Atom" href="https://www.dr-chuck.com/csev-blog/atom.xml" />
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="google-translate-customization" content="502d2c1a267d1206-8efe060c714e194c-g94a06c6c571083ae-11"></meta>
 </head>
 <body>
-<table border=0>
+<a href="#main-content" class="skip-link">Skip to main content</a>
+<table border="0" role="presentation">
 <tr>
 <td align=center valign=top width=180>
-<a href="https://www.learnerprivacy.org/">
-<img align="center" src="Chuck_Square_B&W.jpg" width="160" style="border: white 2px solid;" alt="Photo Credit: Brent Severance">
+<a href="https://www.learnerprivacy.org/" aria-label="Photo of Dr. Charles Severance - LearnerPrivacy.org">
+<img align="center" src="Chuck_Square_B&amp;W.jpg" width="160" style="border: white 2px solid;" alt="Photo of Dr. Charles R. Severance">
 </a>
 <br>
-<br> <a href="https://www.si.umich.edu/" target="_blank">School of Information</a>
+<br> <a href="https://www.si.umich.edu/" target="_blank" rel="noopener noreferrer">School of Information</a>
 <br> <a href="https://online.dr-chuck.com/">Free Courses</a>
-<br> <a href="https://podcast.dr-chuck.com/" target=_blank>Podcast</a>
-<br> <a href="https://youtube.dr-chuck.com/" target=_blank>YouTube</a>
-<br/> <a href="https://www.linkedin.com/in/charlesseverance/" target="_blank">LinkedIn</a>
-<br/> <a target="_blank" href="https://www.coursera.org/instructor/drchuck">My Coursera Page</a>
-<br> <a target="_blank" href="https://amzn.to/1K5Q81K">Amazon Author Page</a>
-<br> <a href="https://www.dr-chuck.com/dr-chuck/resume/speaking.htm" target=_blank>Keynote Speaker</a>
-<br/> <a href="https://www.slideshare.net/csev" target="_blank">Slideshare</a>
-<br> <a href=/dr-chuck/resume/index.htm target=_blank>Resume and Bio</a>
-<br> <a href="https://www.youtube.com/playlist?list=PLHJB2bhmgB7dFuY7HmrXLj5BmHGKTD-3R" target="_blank">IEEE Video Interviews</a>
-<br> <a href=https://www.dr-chuck.com/csev-blog/>Blog</a>
-<br> <a href="http://afs.dr-chuck.com/papers/" target=_blank>Chuck's Papers</a>
-<br> <a href="https://vimeo.com/drchuck/videos" target=_blank>Video on Vimeo</a>
+<br> <a href="https://podcast.dr-chuck.com/" target="_blank" rel="noopener noreferrer">Podcast</a>
+<br> <a href="https://youtube.dr-chuck.com/" target="_blank" rel="noopener noreferrer">YouTube</a>
+<br/> <a href="https://www.linkedin.com/in/charlesseverance/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+<br/> <a target="_blank" rel="noopener noreferrer" href="https://www.coursera.org/instructor/drchuck">My Coursera Page</a>
+<br> <a target="_blank" rel="noopener noreferrer" href="https://amzn.to/1K5Q81K">Amazon Author Page</a>
+<br> <a href="https://www.dr-chuck.com/dr-chuck/resume/speaking.htm" target="_blank" rel="noopener noreferrer">Keynote Speaker</a>
+<br/> <a href="https://www.slideshare.net/csev" target="_blank" rel="noopener noreferrer">Slideshare</a>
+<br> <a href="/dr-chuck/resume/index.htm" target="_blank" rel="noopener noreferrer">Resume and Bio</a>
+<br> <a href="https://www.youtube.com/playlist?list=PLHJB2bhmgB7dFuY7HmrXLj5BmHGKTD-3R" target="_blank" rel="noopener noreferrer">IEEE Video Interviews</a>
+<br> <a href="https://www.dr-chuck.com/csev-blog/">Blog</a>
+<br> <a href="http://afs.dr-chuck.com/papers/" target="_blank" rel="noopener noreferrer">Chuck's Papers</a>
+<br> <a href="https://vimeo.com/drchuck/videos" target="_blank" rel="noopener noreferrer">Video on Vimeo</a>
 </td>
-<td valign=top>
+<td valign="top" id="main-content" tabindex="-1">
+<h1 class="visually-hidden">Dr. Charles R. Severance Home Page</h1>
 <!--
 <strong>
 New:
 </strong>
 -->
+<h2>Free Courses / Educational Material</h2>
 <p>
-Free Courses / Educational Material:
 <br> &nbsp;
-<a href="https://ihts.pr4e.com/" target="_blank">Internet History, Technnology and Security</a>
+<a href="https://ihts.pr4e.com/" target="_blank" rel="noopener noreferrer">Internet History, Technnology and Security</a>
 <br> &nbsp;
-<a href="https://www.py4e.com/" target="_blank">Python for Everybody</a>
+<a href="https://www.py4e.com/" target="_blank" rel="noopener noreferrer">Python for Everybody</a>
 <br> &nbsp;
-<a href="https://www.dj4e.com/" target="_blank">Django for Everybody</a>
+<a href="https://www.dj4e.com/" target="_blank" rel="noopener noreferrer">Django for Everybody</a>
 <br> &nbsp;
-<a href="https://www.wa4e.com/" target="_blank">Web Applications for Everybody</a> (PHP/SQL)
+<a href="https://www.wa4e.com/" target="_blank" rel="noopener noreferrer">Web Applications for Everybody</a> (PHP/SQL)
 <br> &nbsp;
-<a href="https://www.pg4e.com/" target="_blank">PostgreSQL for Everybody</a>
+<a href="https://www.pg4e.com/" target="_blank" rel="noopener noreferrer">PostgreSQL for Everybody</a>
 <br> &nbsp;
-<a href="https://www.cc4e.com/" target="_blank">C Programming for Everybody</a>
+<a href="https://www.cc4e.com/" target="_blank" rel="noopener noreferrer">C Programming for Everybody</a>
 <br> &nbsp;
-<a href="https://www.ca4e.com/" target="_blank">Computer Architecture for Everybody</a>
+<a href="https://www.ca4e.com/" target="_blank" rel="noopener noreferrer">Computer Architecture for Everybody</a>
 <br> &nbsp;
-<a href="/office" target="_blank">Dr. Chuck Office Hours Around the World</a> <br/>
+<a href="/office" target="_blank" rel="noopener noreferrer">Dr. Chuck Office Hours Around the World</a> <br/>
 </p>
+<h2>Software</h2>
 <p>
-Software
 <br> &nbsp;
-<a href="https://www.sakaiproject.org/" target="_blank">The Sakai Collaboration
+<a href="https://www.sakaiproject.org/" target="_blank" rel="noopener noreferrer">The Sakai Collaboration
 and Learning Environment</a>
 <br> &nbsp;
-<a href="https://www.tsugi.org/" target="_blank">Tsugi: A framework for learning tools</a>
+<a href="https://www.tsugi.org/" target="_blank" rel="noopener noreferrer">Tsugi: A framework for learning tools</a>
 <br> &nbsp;
-<a href="https://www.imsglobal.org/activity/learning-tools-interoperability-lti%C2%AE" target="_blank">Learning Tools Interoperability (LTI)</a>
-Interoperability</a>
+<a href="https://www.imsglobal.org/activity/learning-tools-interoperability-lti%C2%AE" target="_blank" rel="noopener noreferrer">Learning Tools Interoperability (LTI)</a>
 </p>
+<h2>Books</h2>
 <p>
-Books
-<br> &nbsp; <a href="http://www.py4e.com/book" /target="_blank">Python For Everybody: Exploring Data in Python 3</a>  (2016)
+<br> &nbsp; <a href="http://www.py4e.com/book" target="_blank" rel="noopener noreferrer">Python For Everybody: Exploring Data in Python 3</a>  (2016)
 <br> &nbsp; <a href="/sakai-book">Sakai: Building an Open Source Community</a> (2011, 2014)
-<br> &nbsp; <a href="http://www.amazon.com/gp/product/1624311393/ref=as_li_ss_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=1624311393&linkCode=as2&tag=drchu02-20" target="_blank">Raspberry Pi (21st Century Skills Innovation Library)</a> (2013)
-<br> &nbsp; <a href="http://www.amazon.com/gp/product/059680069X/ref=as_li_ss_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=059680069X&linkCode=as2&tag=drchu02-20" target="_blank">Using Google App Engine</a> (O'Reilly 2009)
-<br> &nbsp; <a href="http://www.amazon.com/Performance-Computing-Architectures-Optimization-Benchmarks/dp/156592312X/" target="_blank">High Performance Computing</a> (<a href="http://oreilly.com/catalog/9781565923126/" target="_blank">O'Reilly 1998</a>,  <a href="http://cnx.org/content/col11136/latest/" target=_blank>Connexions 2010</a>)
+<br> &nbsp; <a href="http://www.amazon.com/gp/product/1624311393/ref=as_li_ss_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=1624311393&linkCode=as2&tag=drchu02-20" target="_blank" rel="noopener noreferrer">Raspberry Pi (21st Century Skills Innovation Library)</a> (2013)
+<br> &nbsp; <a href="http://www.amazon.com/gp/product/059680069X/ref=as_li_ss_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=059680069X&linkCode=as2&tag=drchu02-20" target="_blank" rel="noopener noreferrer">Using Google App Engine</a> (O'Reilly 2009)
+<br> &nbsp; <a href="http://www.amazon.com/Performance-Computing-Architectures-Optimization-Benchmarks/dp/156592312X/" target="_blank" rel="noopener noreferrer">High Performance Computing</a> (<a href="http://oreilly.com/catalog/9781565923126/" target="_blank" rel="noopener noreferrer">O'Reilly 1998</a>,  <a href="http://cnx.org/content/col11136/latest/" target="_blank" rel="noopener noreferrer">Connexions 2010</a>)
 </p>
+<h2>Web/Multimedia sites</h2>
 <p>
-Web/Multimedia sites
 <br> &nbsp;
-<a href="https://www.learnerprivacy.org/" target="_blank">My LearnerPrivacy.org podcast</a>
+<a href="https://www.learnerprivacy.org/" target="_blank" rel="noopener noreferrer">My LearnerPrivacy.org podcast</a>
 <br> &nbsp;
-<a href="http://www.youtube.com/playlist?list=PLHJB2bhmgB7dFuY7HmrXLj5BmHGKTD-3R" target="_blank">
+<a href="http://www.youtube.com/playlist?list=PLHJB2bhmgB7dFuY7HmrXLj5BmHGKTD-3R" target="_blank" rel="noopener noreferrer">
 IEEE Computer - Computing Conversations Interviews</a>
 (2011-2016)
 <br> &nbsp;
-<a href=https://www.vimeo.com/17207620 target="_blank">
+<a href="https://www.vimeo.com/17207620" target="_blank" rel="noopener noreferrer">
 Dr. Chuck sings the blues </a> (2008)
 <br> &nbsp;
-<a href="https://www.youtube.com/watch?v=BVKpW02hsrU" target="_blank">
+<a href="https://www.youtube.com/watch?v=BVKpW02hsrU" target="_blank" rel="noopener noreferrer">
 Dr. Chuck goes motocross racing</a> (2007)
 <br> &nbsp;
-<a href="https://www.youtube.com/watch?v=sa2WsgCvn7c" target="_blank">
+<a href="https://www.youtube.com/watch?v=sa2WsgCvn7c" target="_blank" rel="noopener noreferrer">
 A Film About Brent and His ATV
 </a> (2005)
 <br> &nbsp;
-<a href="https://www.vimeo.com/17213019" target="_blank">
+<a href="https://www.vimeo.com/17213019" target="_blank" rel="noopener noreferrer">
 Audition Tape</a> (2003) for TechTV which was
 rejected :(.
 <br> &nbsp;
-<a href="https://www.youtube.com/watch?v=FJ078sO35M0" target="_blank">
+<a href="https://www.youtube.com/watch?v=FJ078sO35M0" target="_blank" rel="noopener noreferrer">
 Dr. Chuck goes stock car racing</a> (2002)
 <br> &nbsp;
-<a href="http://afs.dr-chuck.com/citoolkit" target=_blank>
+<a href="http://afs.dr-chuck.com/citoolkit" target="_blank" rel="noopener noreferrer">
 The Community Information Toolkit</a> - A project to provide public libraries and 
 other organizations a start on using Internet in Commmunity Networking. (1999)
 </td>
 </tr>
 </table>
 <br> &nbsp;
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-423997-1']);
-  _gaq.push(['_setDomainName', 'dr-chuck.com']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 </body>
 </html>
